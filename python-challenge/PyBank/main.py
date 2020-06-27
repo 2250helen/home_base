8f8685894255eca4fda103e7aa3bbbc5d7aa3746
2250helen <2250helen@gmail.com>
2020-06-27 23:32:33 +0800

    python challenge 3rd refresh

--- python-challenge/PyBank/main.py
+++ python-challenge/PyBank/main.py
@@ -32,20 +32,7 @@ with open(pybank, newline='') as csvfile:
         #add value to net amount
         totalRevenue += int(row[1])
         
-        #if next row's revenue value is positive,
-        if (int(row[1]) > 0):
-            #compare it to the current greatest profit value, if greater,
-            if (int(row[1]) > greatestProfit):
-                #replace the greatest profit value and date
-                greatestProfitDate = row[0]
-                greatestProfit = int(row[1])
-        #if next row's revenue value is negative,
-        if (int(row[1]) < 0):
-            #compare it to the current greatest loss value, if less,
-            if (int(row[1]) < greatestLoss):
-                #replace the greatest loss value and date
-                greatestLossDate = row[0]
-                greatestLoss = int(row[1])
+     
 
 #output statements
 print("Financial Analysis")
